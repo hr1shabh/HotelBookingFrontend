0d43800f37587d02c3eaf5c8acd8ed8e15a75b41
hr1shabh <hrishabhpalsra@gmail.com>
2023-12-13 10:21:47 +0530

    changed link to navigate

--- src/components/searchItem/SearchItem.jsx
+++ src/components/searchItem/SearchItem.jsx
@@ -1,7 +1,11 @@
-import { Link } from 'react-router-dom'
-import './searchItem.css'
-
+import { useNavigate } from 'react-router-dom';import './searchItem.css'
 const SearchItem = ({item}) => {
+    const navigate = useNavigate();
+
+    const handleButtonClick = () => {
+      navigate(`/hotels/${item._id}`);
+    };
+  
   return (
     <div className='searchItem'>
         <img src={item.photos[0]} className='siImg'></img>
@@ -28,9 +32,7 @@ const SearchItem = ({item}) => {
             <div className='siDetailTexts'>
                 <span className='siPrice'>Rs.{item.cheapestPrice}</span>
                 <span className='siTaxOp'>Includes taxes and fees</span>
-                <Link to={`/hotels/${item._id}`}>
-                <button className='siCheckButton'>see availability</button>
-                </Link>
+                <button className='siCheckButton' onClick={handleButtonClick}>see availability</button>
             </div>
         </div>
     </div>
